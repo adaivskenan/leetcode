64766e7a338df2b22b354a6c149a28453ef7d631
adaivskenan <adaivskenan@foxmail.com.cn>
2019-04-04 17:24:12 +0800

    hamming distance 100.00%

---
+++ algorithms/java/src/hammingDistance/HammingDistance.java
@@ -0,0 +1,40 @@
+//https://leetcode-cn.com/problems/hamming-distance/
+package hammingDistance;
+
+/**
+ * The Hamming distance between two integers is the number of positions at which
+ * the corresponding bits are different.
+ * 
+ * Given two integers x and y, calculate the Hamming distance.
+ * 
+ * Note: 0 ≤ x, y < 231.
+ * 
+ * Example:
+ * 
+ * Input: x = 1, y = 4
+ * 
+ * Output: 2
+ * 
+ * Explanation: 1 (0 0 0 1) 4 (0 1 0 0) ↑ ↑
+ * 
+ * The above arrows point to positions where the corresponding bits are
+ * different.
+ */
+public class HammingDistance {
+
+	public static void main(String[] args) {
+		System.out.println(hammingDistance(1, 4));
+	}
+
+	public static int hammingDistance(int x, int y) {
+		int dif = x ^ y;
+		int num = 0;
+		while (dif != 0) {
+			num = num + (dif & 1);
+			dif = dif >> 1;
+		}
+		return num;
+
+	}
+
+}
